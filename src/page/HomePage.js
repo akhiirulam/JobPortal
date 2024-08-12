6e73dfa29fb4d173baaaec48ea32370481150351
akhiirulam <akhhiirulam@gmail.com>
2024-08-12 22:39:00 +0530

    0.1

---
+++ src/page/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+        <div className="container mx-auto md:w-[1320px]">
+            HomePage
+        </div>
+    </div>
+  );
+};
+
+export default HomePage
