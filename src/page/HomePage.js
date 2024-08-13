5fb616b793155b566c3f7fd793632a2310796a6e
akhiirulam <akhhiirulam@gmail.com>
2024-08-13 15:00:24 +0530

    v1.0

--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -2,8 +2,6 @@ import React from "react";
 import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
 
-
-
 const HomePage = () => {
   return (
       <div className="w-full">
