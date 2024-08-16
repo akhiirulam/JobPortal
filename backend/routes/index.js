65dc956531a3687f1fe95154e6738b46323e936f
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-08-16 20:04:06 +0530

    job route added to index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const auth = require('./auth-Route')
 const userRoutes = require('./user-Route')
 const userProfile = require('./userProfile-Route');
+const jobRoutes = require('./job-route');
 
-router.use('/user',userRoutes);
-router.use('/profile',userProfile)
-router.use('/otp',auth)
+router.use('/user', userRoutes);
+router.use('/profile', userProfile)
+router.use('/job', jobRoutes)
+router.use('/otp', auth)
 
 module.exports = router;
