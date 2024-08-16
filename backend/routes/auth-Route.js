ab341a228e919de28d4647b363a3e51eae394c3e
akhiirulam <akhhiirulam@gmail.com>
2024-08-16 20:04:10 +0530

    v0.2.1

--- backend/routes/auth-Route.js
+++ backend/routes/auth-Route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController'); // Relative path
 
-
 router.post('/login', userController.login);
 router.post('/otpGeneration',userController.otpGeneration)
 router.post('/otpVerification',userController.otpVerification)
