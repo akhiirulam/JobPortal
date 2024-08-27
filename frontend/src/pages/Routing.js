18a5ed11b56daac24e9a810557b0f193221a75c6
Basil Alias <basilalyas2000@gmail.com>
2024-08-27 15:41:04 +0530

    Completed deleteProfile,changePassword,employerAppliedJobs

--- frontend/src/pages/Routing.js
+++ frontend/src/pages/Routing.js
@@ -4,6 +4,9 @@ import HomePage from "./HomePage";
 import Login from "../components/Login/Login";
 import Employer from "./Employer";
 import CandidateAppliedJobs from "../components/candidateAppliedJobs/CandidateAppliedJobs";
+import EmployerCandidateAlert from "../components/employerCandidateAlerts/EmployerCandidateAlert";
+import ChangePassword from "../components/changePassword/ChangePassword";
+import DeleteProfile from "../components/deleteProfile/DeleteProfile";
 
 function Landing() {
   return (
@@ -13,6 +16,9 @@ function Landing() {
         <Route path="/login" element={<Login />} />
         <Route path="/employer" element = {<Employer/>} />
         <Route path="/candidate/applied-jobs" element={<CandidateAppliedJobs/>} />
+        <Route path="/employer/candidate-alerts" element={<EmployerCandidateAlert/>} />
+        <Route path="/change-password" element={<ChangePassword/>} />
+        <Route path="/delete-profile" element={<DeleteProfile/>} />
       </Routes>
     </Router>
   );
