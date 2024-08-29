df7ae8f7f5731b258b01bbaee0f599cd7219f6ff
Basil Alias <basilalyas2000@gmail.com>
2024-08-29 21:29:59 +0530

    Merged changes and resolved conflicts

--- backend/routes/job-route.js
+++ backend/routes/job-route.js
@@ -2,33 +2,15 @@ const express = require('express');
 const router = express.Router();
 const jobController = require('../controllers/jobController');
 const isAuth = require('../middlewares/auth');
-const multer = require('multer');
 
-// Set up multer for file uploads
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Set the upload directory
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname); // Set the filename
-    }
-});
-
-const upload = multer({ storage });
 
 // Route to add job details
-router.post('/add', upload.single('featuredImage'),isAuth, jobController.addJobDetails);
+router.post('/add', jobController.addJobDetails);
 
 // Route to edit job details
 router.put('/edit/:id', isAuth, jobController.editJobDetails);
 
 // Route to delete job details
 router.delete('/delete/:id', isAuth, jobController.deleteJobDetails);
-
-//list all jobs 
-router.get('/jobs',isAuth, jobController.listJobs);
-
-//filtering jobs
-router.get('/job-filter', jobController.filterJobs)
 
 module.exports = router;
